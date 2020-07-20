faf147a9a3f9cabe4bd063139558074cd5ffc54e
Cyrus-test <402879338@qq.com>
2020-07-20 23:13:00 +0800

    day11_22_名片管理系统_tools

---
+++ day11_22_名片管理系统_tools.py
@@ -0,0 +1,45 @@
+card_list = []
+
+def show_menu():
+    """显示菜单"""
+    print("*" * 50)
+    print("欢迎使用【名片管理系统】v 1.0")
+    print("")
+    print("1.新增名片")
+    print("2.显示全部")
+    print("3.搜索名片")
+    print("")
+    print("0.退出名片管理系统")
+    print("*" * 50)
+
+def new_card():
+    """新增名片"""
+    print("-" * 50)
+    print("新增名片")
+    # 1.提示用户输入名片的详细信息
+    name = input("请输入姓名：")
+    phone = input("请输入电话：")
+    qq = input("请输入QQ:")
+    email = input("请输入邮箱：")
+    # 2.使用用户提示的信息建立一个名片字典
+    card_dict = {"name": name,
+                 "phone": phone,
+                 "qq": qq,
+                 "email": email}
+    # 3.将名片字典加入到列表中
+    card_list.append(card_dict)
+    print(card_list)
+
+    # 4.提示用户添加成功
+    print("添加 %s 的名片成功！" % name)
+
+
+def show_all():
+    """显示全部"""
+    print("-" * 50)
+    print("显示全部")
+
+def search_card():
+    """搜索名片"""
+    print("-" * 50)
+    print("搜索名片")
