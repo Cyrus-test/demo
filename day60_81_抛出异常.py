fa2cfd80ec5f1402a34b5cf08f2c4ab156b04e01
Cyrus-test <402879338@qq.com>
2020-09-06 23:07:53 +0800

    day60_81_抛出异常

---
+++ day60_81_抛出异常.py
@@ -0,0 +1,21 @@
+def input_pwd():
+    # 1.提示用户输入密码
+    pwd = input("请输入密码： ")
+
+    # 2.判断密码长度>= 8 ,返回用户输入的密码
+    if len(pwd) >= 8:
+        return pwd
+
+    # 3.如果<8 主动抛出异常
+    print("密码输入异常 ")
+    # <1> 1.创建异常对象
+    ex = Exception("密码长度不够")
+    # <2> 2.主动抛出异常
+    raise ex
+
+
+# 提示用户输入密码
+try:
+    print(input_pwd())
+except Exception as result:
+    print(result)
