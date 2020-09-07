0add32a80ed2898b019e2b030ab6fa1e807f8d21
Cyrus-test <402879338@qq.com>
2020-09-07 22:55:28 +0800

    day61_82_模块

---
+++ day61_82_模块.py
@@ -0,0 +1,12 @@
+import day61_82_测试模块1
+import dya61_82_测试模块2
+
+
+day61_82_测试模块1.say_hello()
+dya61_82_测试模块2.say_hello()
+
+dag = day61_82_测试模块1.Dag()
+print(dag)
+
+cat = dya61_82_测试模块2.Cat()
+print(cat)
