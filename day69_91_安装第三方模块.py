cef12ebad0138a437eb5dee8fa5d90af4852ec53
Cyrus-test <402879338@qq.com>
2020-09-16 22:55:18 +0800

    day69_91_安装第三方模块

---
+++ day69_91_安装第三方模块.py
@@ -0,0 +1,14 @@
+# 将模块安装到python2.x环境
+# $ sudo pip insatll pygame
+# $ sudo pip uninstall pygame
+#
+# 将模块安装到python3.x环境
+# $ sudo pip3 insatll pygame
+# $ sudo pip3 uninstall pygame
+#
+# 在mac电脑安装ipython
+# $ sudo pip install ipython
+#
+# 在Linux下安装ipython
+# $ sudo apt install ipython
+# $ sudo apt install ipython3
