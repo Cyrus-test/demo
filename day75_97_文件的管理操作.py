ce069269c6fb372ef54cba9deaf600fc4b9bc05e
Cyrus-test <402879338@qq.com>
2020-09-22 23:54:12 +0800

    day75_97_文件的管理操作

---
+++ day75_97_文件的管理操作.py
@@ -0,0 +1,10 @@
+# 在python中，如果希望通过程序实现，创建，重命名，删除，修改路径，需要导入os模块
+#
+# os.rename 重命名文件
+# os.remove 删除文件
+# os.listdir 目录列表
+# os.mkdir 创建目录
+# os.rmdir 删除目录
+# os.getcwd 获取当前目录
+# os.chdir 修改工作目录
+# os.path isdir 判断是否是文件
