191325e072b849b44e9f4010d289775a37685597
Cyrus-test <402879338@qq.com>
2020-09-24 22:57:17 +0800

    day76_98_文本文件的编码格式

---
+++ day77_100_eval.py
@@ -0,0 +1,3 @@
+# eval 会把字符串的结果返回给计算机，把引号去掉
+input_str = input("请输入计算题： ")
+print(eval(input_str))
---
+++ day77_99_python2字符串.py
@@ -0,0 +1,5 @@
+# 想在python2.x中使用中文字符
+# *-* coding:utf8 *-*
+hello_str = "hello world"
+
+print(hello_str)
